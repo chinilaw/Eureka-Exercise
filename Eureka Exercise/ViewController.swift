380b55a566df8ae32a384ad6b5585c06a8d6cf9a
Jules Lee <jackconsolidated@gmail.com>
2019-08-03 13:12:43 +0800

    Initial Commit

---
+++ Eureka Exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Eureka Exercise
+//
+//  Created by Jules Lee on 03/08/2019.
+//  Copyright © 2019 Jules Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
